5e044dc4611b89035973d4a4c8126a17275d8e0e
zhaolun <1067112004@qq.com>
2014-12-17 20:55:57 +0800

    1

--- Admin/Home/Conf/config.php
+++ Admin/Home/Conf/config.php
@@ -1,4 +1,12 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DB_NAME'   => 'bbs', // 数据库名
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => '940614', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => 'bbs_', // 数据库表前缀
+	'DB_CHARSET'=> 'utf8', //字符集
+	'LAYOUT_ON'=>true,
+	'LAYOUT_NAME'=>'index',
 );
