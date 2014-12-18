d22cb0c8c793eba8d9788bdf06351afed01988c2
liujun <liujun@qq.com>
2014-12-18 16:17:31 +0800

    add

--- Admin/Home/Controller/TeacherController.class.php
+++ Admin/Home/Controller/TeacherController.class.php
@@ -19,13 +19,13 @@ class TeacherController extends Controller {
 		$upload = new \Think\Upload();// 实例化上传类
 		$upload->maxSize = 3145728 ;// 设置附件上传大小
 		$upload->exts = array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
-		$upload->rootPath = '/Uploads/'; // 设置附件上传根目录
+		$upload->rootPath = '/Public/Uploads/'; // 设置附件上传根目录
 		// 上传文件
 		$model=M('teacher');
         $data['t_name']=$_POST['site_name'];
         $data['t_desc']=$_POST['position_id'];
         $data['p_id']=$_POST['p_id'];
-		$data['img']='/Uploads/'.$_FILES['filename']['name'];
+		$data['img']='/Public/Uploads/'.$_FILES['filename']['name'];
 		$info = $upload->upload();
 		if($info&&$model->add($data)) {
 			// 上传错误提示错误信息
