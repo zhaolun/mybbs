efe03016ad73fa4dadbe552f374f7d3f174aabc8
cmz <790330816@qq.com>
2014-12-19 15:14:41 +0800

    add

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,10 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        $user = M('family');//获取表总数据
+		$data = $user->select();
+		//print_r($data);die;
+		$this->assign('info',$data);
         $this->display('index');
     }
 	public function jiuye(){
