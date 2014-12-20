ab092296c91a019bf22f2d848d422368adc14a58
cmz <790330816@qq.com>
2014-12-20 11:13:07 +0800

    add

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,9 +4,27 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
         $user = M('family');//获取表总数据
+        $usera = M('xueyuan');
+		$userb = M('banji');
+		$userc = M('zhaolun');
+		$userd = M('liujun');
+		$usere = M('php');
+		$userf = M('mingshi');
 		$data = $user->select();
+		$data1 = $usera->select();
+		$data2 = $userb->select();
+		$data3 = $userc->select();
+		$data4 = $userd->select();
+		$data5 = $usere->select();
+		$data6 = $userf->select();
 		//print_r($data);die;
 		$this->assign('info',$data);
+		$this->assign('infoa',$data1);
+		$this->assign('infob',$data2);
+		$this->assign('infoc',$data3);
+		$this->assign('infod',$data4);
+		$this->assign('infoe',$data5);
+		$this->assign('infof',$data6);
         $this->display('index');
     }
 	public function jiuye(){
