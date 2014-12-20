f45c85a361bb106e32ee562d9ce5a2321fa44b24
zhangxiaoyun <18311171705@163.com>
2014-12-20 08:01:17 +0800

    df

--- Admin/Home/Controller/MessageController.class.php
+++ Admin/Home/Controller/MessageController.class.php
@@ -136,8 +136,6 @@ class MessageController extends Controller {
     	{
     		$this->error("编辑失败");
     	}
-
-
     }
     //显示学院列表
     public function school()
@@ -215,10 +213,9 @@ class MessageController extends Controller {
     public function student()
     {
         $model=M("student");
-        $data=$model->query("select * from bbs_student");
-        //var_dump($data);die;
-        //      $sql = " select * from rec_resume as rec_resume,rec_company as rec_company,rec_user_deliver as rec_user_deliver where rec_company.com_id='$com_id' and rec_user_deliver.add_time between '$addtime' and '$add1time' and rec_resume.res_id=rec_user_deliver.res_id and rec_company.com_id=rec_user_deliver.com_id ";
-
+        $data=$model->query("select * from bbs_student,bbs_school,bbs_company where bbs_student.school=bbs_school.s_id and bbs_student.company=bbs_company.com_id");
+       // var_dump($data);die;
+        $this->assign('list',$data);
     	$this->display();
     }
     //显示添加学生form
@@ -255,6 +252,72 @@ class MessageController extends Controller {
         $stu=$model->add($data);
         //echo "<pre>";var_dump($info['pic']['savename']);die; 
         if($info&&$stu) 
+        {
+            $this->redirect("/admin.php/home/message/student");
+        }else
+        {  
+            $this->error($upload->getError());    
+        }
+    }
+    //删除学生信息
+    public function stu_del()
+    {
+        $stu_id=$_GET['stu_id'];
+        $model=M('student');
+        $del=$model->where("stu_id=$stu_id")->delete();
+        if($del)
+        {
+            $this->redirect("/admin.php/home/message/student");
+        }else
+        {
+            $this->error("删除失败");
+        }
+    }
+    //显示编辑列表
+    public function stu_editform()
+    {
+        $stu_id=$_GET["stu_id"];
+
+        $modle=M("company");
+        $com=$modle->select();
+        $this->assign('com',$com);
+
+        $model1=M('school');
+        $sch=$model1->select();
+        $this->assign('sch',$sch);
+
+        $model=M("student");
+        $data=$model->where("stu_id=$stu_id")->select();
+        //echo "<pre>";var_dump($data);
+        $this->assign("list",$data);
+        $this->display();
+    }
+    //执行编辑
+    public function stu_edit()
+    {
+        //var_dump($_POST['stu_id']);die;
+        $stu_id=$_POST['stu_id'];
+
+
+        $model=M('student');
+        //上传文件
+        $upload=new \Think\Upload();
+        $upload->maxSize = 3145728;
+        $upload->saveName = 'time';
+        $upload->exts= array('jpg','gif','png','jpeg');// 设置附件上传类型    
+        $upload->savePath  ='/Uploads';//设置附件上传目录
+        $info=$upload->upload();
+        //执行添加
+        $data['stu_name']=$_POST['stu_name'];
+        $data['school']=$_POST['school'];
+        $data['work_time']=$_POST['work_time'];
+        $data['company']=$_POST['company'];
+        $data['money']=$_POST['money'];
+        $data['pic']=$info['pic']['savename'];//获取文件保存的名称
+        //$stu=$model->add($data);
+        $update=$model->where("stu_id=$stu_id")->save($data);
+        //echo "<pre>";var_dump($info['pic']['savename']);die; 
+        if($info&&$update) 
         {
             $this->redirect("/admin.php/home/message/student");
         }else
