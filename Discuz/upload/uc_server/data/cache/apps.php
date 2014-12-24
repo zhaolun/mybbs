20f2d5632302039fa32ad56611e39ac824c74fc0
cmz <790330816@qq.com>
2014-12-24 10:31:47 +0800

    DD

---
+++ Discuz/upload/uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://www.mybbs.com/discuz/upload',
+    'authkey' => 'f6g26f3eM8waQ9qdJ3F6O3J8f73c7d45m8qfTdV3X7kekae3z1Hb496cN0P3kewb',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
