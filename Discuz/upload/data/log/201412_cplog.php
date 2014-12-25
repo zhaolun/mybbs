222e778d318d25bb94d383bb29d534376eddd4be
zhangxiaoyun <18311171705@163.com>
2014-12-25 11:11:56 +0800

    sdfs

---
+++ Discuz/upload/data/log/201412_cplog.php
@@ -0,0 +1,8 @@
+<?PHP exit;?>	1419477025	admin	1	127.0.0.1		GET={}; POST={};
+<?PHP exit;?>	1419477026	admin	1	127.0.0.1	index	GET={}; POST={};
+<?PHP exit;?>	1419477039	admin	1	127.0.0.1	nav	GET={}; POST={};
+<?PHP exit;?>	1419477041	admin	1	127.0.0.1	nav	GET={operation=headernav; do=edit; id=2; }; POST={};
+<?PHP exit;?>	1419477065	admin	1	127.0.0.1	nav	GET={operation=headernav; do=edit; id=2; namenew=IT学习社区; titlenew=BBS; urlnew=forum.php; editsubmit=提交; }; POST={namenew=IT学习社区; titlenew=BBS; urlnew=forum.php; editsubmit=提交; };
+<?PHP exit;?>	1419477069	admin	1	127.0.0.1	nav	GET={operation=headernav; }; POST={};
+<?PHP exit;?>	1419477091	admin	1	127.0.0.1	nav	GET={operation=headernav; displayordernew={2=2; 5=6; 6=9; 7=10; 49=11; 50=12; 51=13; 52=14; 53=15; 54=16; }; namenew={2=IT学习社区; 5=游戏; 6=插件; 7=帮助; 49=IT学习社区; 50=技术论坛; 51=IT问答精灵; 52=传智特刊; 53=积分商城; 54=积分抽奖; }; subtypenew={}; defaultindex=forum.php; availablenew={2=1; 5=1; 51=1; 52=1; 53=1; 54=1; }; urlnew={49=it.php; 50=misc.php?mod=faq; 51=question.php; 52=books.php; 53=www.mybbs.com/Bbs/Home/View/Plugin/plugin.html; 54=choujiang.php; }; }; POST={displayordernew={2=2; 5=6; 6=9; 7=10; 49=11; 50=12; 51=13; 52=14; 53=15; 54=16; }; namenew={2=IT学习社区; 5=游戏; 6=插件; 7=帮助; 49=IT学习社区; 50=技术论坛; 51=IT问答精灵; 52=传智特刊; 53=积分商城; 54=积分抽奖; }; subtypenew={}; defaultindex=forum.php; availablenew={2=1; 5=1; 51=1; 52=1; 53=1; 54=1; }; urlnew={49=it.php; 50=misc.php?mod=faq; 51=question.php; 52=books.php; 53=www.mybbs.com/Bbs/Home/View/Plugin/plugin.html; 54=choujiang.php; }; };
+<?PHP exit;?>	1419477094	admin	1	127.0.0.1	nav	GET={operation=headernav; }; POST={};
