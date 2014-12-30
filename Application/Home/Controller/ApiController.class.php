dde18368cda0fc01d8b3e0c5ebe34da283489ba3
zhaolun <1067112004@qq.com>
2014-12-30 17:18:32 +0800

    1

--- Application/Home/Controller/ApiController.class.php
+++ Application/Home/Controller/ApiController.class.php
@@ -3,7 +3,20 @@ namespace Home\Controller;
 use Think\Controller;
 class ApiController extends Controller {
     public function index(){
-        
+		if(!$this->keys($_GET['key'])){
+			echo $_GET['callback']."(".json_encode(['error'=>"秘钥不正确"]).")";
+			die;
+		}
+		$db=M("teacher");
+		$list=$db->limit($_GET['count'])->select();
+        echo $_GET['callback']."(".json_encode($list).")";
     }
 
+	public function keys($key){
+		$rekek=md5("123456");
+		if( $rekek == $key )
+			return true;
+		else
+			return false;
+	}
 }
